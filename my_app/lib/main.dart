a8fda12e68b8b8d1aeebbd3634fe826f24521f30
Soliman <soliman@uni-bremen.de>
2025-05-28 04:28:29 +0200

    flutter app

---
+++ my_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+      return MaterialApp(
+          initialRoute: '/',
+          routes: {
+              '/':     (ctx) => HomeScreen(),
+              '/about':(ctx) => AboutScreen(),
+          },
+      );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Route')),
+      body: Center(
+        child: ElevatedButton(
+          key: const Key('toAbout'),
+          onPressed: () => Navigator.pushNamed(context, '/about'),
+          child: const Text('Go to About'),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('About Route')),
+      body: Center(
+        child: ElevatedButton(
+          key: const Key('toHome'),
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Back to Home'),
+        ),
+      ),
+    );
+  }
+}
